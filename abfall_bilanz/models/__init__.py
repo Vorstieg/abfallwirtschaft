dc36a44cc8e83ce221d8117cf73729abe056ca8c
Benjamin Wieser <benjamin.wieser@vorstieg.eu>
2025-09-26 16:05:47 +0200

    add reconciliation models

--- abfall_bilanz/models/__init__.py
+++ abfall_bilanz/models/__init__.py
@@ -2,3 +2,5 @@
 
 from . import waste_bilanz
 from . import waste_move
+from . import reconciliation_rules
+from . import stock_picking
--- abfall_bilanz/models/waste_move.py
+++ abfall_bilanz/models/waste_move.py
@@ -5,7 +5,6 @@ class WasteMove(models.Model):
 
     name = fields.Char(string='Waste Move',)
 
-    transport_type = fields.Many2one('waste.transport.type', string='Transport Type')
     recipient_partner = fields.Many2one('res.partner', string='Recipient Partner')
     origin_partner = fields.Many2one('res.partner', string='Origin Partner')
 
@@ -14,13 +13,18 @@ class WasteMove(models.Model):
     recipient_site = fields.Many2one('waste.treatment.site', string='Recipient Site')
     origin_site = fields.Many2one('waste.treatment.site', string='Origin Site')
 
+    recycling_type = fields.Many2one('waste.recycling.type', string='Recycling Type')
+    origin_type = fields.Many2one('waste.origin.type', string='Origin Type')
+    transport_type = fields.Many2one('waste.transport.type', string='Transport Type')
+
     abfallart = fields.Many2one('waste.type', "Abfallart")
     amount = fields.Float(string='Transferred amount')
+    quantification_type = fields.Many2one('waste.quantification.type', string='Transferred amount')
 
     date = fields.Datetime(string='Date', default=fields.Datetime.now)
     state = fields.Selection(
         [('draft', 'New'), ('cancel', 'Cancelled'), ('approved', 'Approved')],
-        'Status', copy=False, default='draft', index=True)
+        'Status', copy=False, default='draft')
 
     def action_show_dialog(self):
         return {'type': 'ir.actions.act_window',
